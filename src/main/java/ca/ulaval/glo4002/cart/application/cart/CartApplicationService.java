c3157eb8b389850298c4e4142a9aabe0d2145d0c
RphlBrdt <raphael.bardot@gmail.com>
2018-09-21 09:55:04 -0400

    commit base

---
+++ src/main/java/ca/ulaval/glo4002/cart/application/cart/CartApplicationService.java
@@ -0,0 +1,47 @@
+package ca.ulaval.glo4002.cart.application.cart;
+
+import java.util.List;
+
+import ca.ulaval.glo4002.cart.domain.cart.Cart;
+import ca.ulaval.glo4002.cart.domain.cart.CartItem;
+import ca.ulaval.glo4002.cart.domain.shop.ShopItem;
+
+public class CartApplicationService {
+
+    public static final int SHIPPING_PRICE_PER_KG = 2;
+    private CartRepository cartRepository;
+
+	public CartApplicationService() {
+		this.cartRepository = new CartRepository();
+	}
+
+	public Cart findOrCreateCartForClient(String email) {
+		List<Cart> carts = cartRepository.retrieveCarts();
+
+		Cart cart = getCartByOwner(email, carts);
+
+		return cart;
+	}
+
+	public void addItemToCart(String email, ShopItem item) {
+		List<Cart> carts = cartRepository.retrieveCarts();
+		Cart cart = getCartByOwner(email, carts);
+
+		cart.addItem(new CartItem(item.getName(), 1, getItemPriceWithShipping(item)));
+
+		cartRepository.persistCarts(carts);
+	}
+
+    private int getItemPriceWithShipping(ShopItem item) {
+        return item.getPrice();
+    }
+
+    private Cart getCartByOwner(String email, List<Cart> carts) {
+		return carts.stream().filter(c -> c.ownerEmail.equals(email)).findFirst().orElseGet(() -> {
+			Cart newCart = new Cart(email);
+			carts.add(newCart);
+			cartRepository.persistCarts(carts);
+			return newCart;
+		});
+	}
+}
---
+++ src/main/java/ca/ulaval/glo4002/cart/domain/shop/ShopItem.java
@@ -0,0 +1,18 @@
+package ca.ulaval.glo4002.cart.domain.shop;
+
+import javax.xml.bind.annotation.XmlSeeAlso;
+
+@XmlSeeAlso({StandardShopItem.class, PrimeShopItem.class, FragileShopItem.class})
+public abstract class ShopItem {
+    public abstract String getName();
+
+    public abstract boolean isAvailable();
+
+    public abstract boolean hasSku(String sku);
+
+    public abstract int getPrice();
+
+    public abstract int getWeight();
+
+    public abstract boolean isPrime();
+}
